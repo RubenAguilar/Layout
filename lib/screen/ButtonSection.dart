e44221b70cdc14ed20aaddfd1fadf9c5f4a7866d
Ruben Aguilar <ruben.aguilar@itrends.mx>
2024-02-28 03:01:16 -0600

    seccion de botones

--- lib/screen/ButtonSection.dart
+++ lib/screen/ButtonSection.dart
@@ -46,7 +46,21 @@ class ButtonWithText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-// ···
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
